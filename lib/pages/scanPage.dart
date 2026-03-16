7ba99498c3b3312cb7ecb5d7c6f701057010081d
Mohammad Asadullah <87116101+Asadullah2412@users.noreply.github.com>
2026-03-16 21:49:14 +0530

    added leaf_model

--- lib/pages/scanPage.dart
+++ lib/pages/scanPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'dart:typed_data';
+import 'package:image/image.dart' as img;
+import '../services/inference_service.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -12,15 +15,70 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> {
   File? imageFile;
   final ImagePicker picker = ImagePicker();
+  InferenceService inferenceService = InferenceService();
 
+// image preprocess
+  Future<List> preprocessImage(File imageFile) async {
+    final bytes = await imageFile.readAsBytes();
+
+    img.Image? original = img.decodeImage(bytes);
+
+    img.Image resized = img.copyResize(original!, width: 224, height: 224);
+
+    List input = List.generate(
+      1,
+      (_) => List.generate(
+        224,
+        (y) => List.generate(
+          224,
+          (x) {
+            final pixel = resized.getPixel(x, y);
+
+            return [pixel.r / 255.0, pixel.g / 255.0, pixel.b / 255.0];
+          },
+        ),
+      ),
+    );
+
+    return input;
+  }
+
+// pick image
   Future pickImage() async {
     final XFile? image = await picker.pickImage(source: ImageSource.camera);
 
     if (image != null) {
+      File file = File(image.path);
+
       setState(() {
-        imageFile = File(image.path);
+        imageFile = file;
       });
+
+      List input = await preprocessImage(file);
+
+      bool leaf = inferenceService.isLeaf(input);
+
+      if (!leaf) {
+        print("Not a leaf image");
+        return;
+      }
+
+      List<double> output = inferenceService.runInference(input);
+
+      int index = inferenceService.getMaxIndex(output);
+
+      List<String> labels = await inferenceService.loadLabels();
+
+      String prediction = labels[index];
+
+      print("Prediction: $prediction");
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    inferenceService.loadModel();
   }
 
   @override
--- lib/services/inference_service.dart
+++ lib/services/inference_service.dart
@@ -1,36 +1,54 @@
-import 'package:flutter/services.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:flutter/services.dart';
 
 class InferenceService {
   late Interpreter interpreter;
-  late List<String> labels;
+  late Interpreter leafInterpreter;
 
   Future loadModel() async {
-    interpreter = await Interpreter.fromAsset('models/cipherplant.tflite');
-
-    final labelData = await rootBundle.loadString('assets/labels/labels.txt');
-    labels = labelData.split('\n');
-    print("Input Shape: ${interpreter.getInputTensor(0).shape}");
-    print("Output Shape: ${interpreter.getOutputTensor(0).shape}");
+    interpreter =
+        await Interpreter.fromAsset('assets/models/cipherplant.tflite');
+    leafInterpreter =
+        await Interpreter.fromAsset('assets/models/leaf_model.tflite');
+    print("Models loaded successfully");
   }
 
-  Future<Map<String, dynamic>> predict(List inputTensor) async {
-    final output = List.generate(1, (_) => List.filled(labels.length, 0.0));
+  bool isLeaf(List input) {
+    var output = List.filled(1 * 1, 0.0).reshape([1, 1]);
 
-    interpreter.run(inputTensor, output);
+    leafInterpreter.run(input, output);
 
-    final scores = output[0];
+    double probability = output[0][0];
 
-    double maxScore = scores[0];
+    print("Leaf probability: $probability");
+
+    return probability > 0.5;
+  }
+
+  int getMaxIndex(List<double> predictions) {
+    double maxVal = predictions[0];
     int maxIndex = 0;
 
-    for (int i = 1; i < scores.length; i++) {
-      if (scores[i] > maxScore) {
-        maxScore = scores[i];
+    for (int i = 1; i < predictions.length; i++) {
+      if (predictions[i] > maxVal) {
+        maxVal = predictions[i];
         maxIndex = i;
       }
     }
 
-    return {"label": labels[maxIndex], "confidence": maxScore};
+    return maxIndex;
+  }
+
+  Future<List<String>> loadLabels() async {
+    final labelsData = await rootBundle.loadString('assets/labels/labels.txt');
+    return labelsData.split('\n');
+  }
+
+  List<double> runInference(List input) {
+    var output = List.filled(1 * 6, 0.0).reshape([1, 6]);
+
+    interpreter.run(input, output);
+
+    return List<double>.from(output[0]);
   }
 }
